640ee539d1ab599794c7b435b6a80b3bae6b791a
Aliza Razi <aliza302001@gmail.com>
2023-08-01 02:58:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.json())
+
+app.post("/login", (req, res) => {
+    //sending json data
+    const { one, two, three } = req.body
+
+    if (!email || !password) {
+        res.json({ error: "All fields are required" })
+    }
+    else {
+        res.json({ success: "Data recieved" })
+    }
+})
+
+app.post("/", (req, res) => {
+    res.json({ message: "This is a json data" })
+})
+
+
+app.listen(5000, () => { console.log("App is running on port 5000") })
